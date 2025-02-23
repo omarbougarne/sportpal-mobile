34fc8d1e2eeb2a5dfb8c9f23d65f5f0c7ee6fda3
omarbougarne <omarbougarne98@gmail.com>
2025-02-23 16:56:25 +0000

    Feat Addedasync storage for managing and persesting user data

--- app/services/api/apiClient.ts
+++ app/services/api/apiClient.ts
@@ -1,9 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import Constants from "expo-constants"
 
 const apiClient = axios.create({
     baseURL: Constants.expoConfig?.extra?.apiUrl,
     timeout: 10000,
-})
+});
+
+
+//Inclusion of token in the header
+apiClient.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('authToken');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default apiClient;
--- app/services/api/authApi.ts
+++ app/services/api/authApi.ts
@@ -1,9 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import apiClient from './apiClient';
 import { SignUpData, LoginData } from '@/app/types/auth';
 
 export const signUp = async (userData: SignUpData) => {
     try {
         const response = await apiClient.post('/auth/signup', userData);
+        const { token } = response.data;
+        await AsyncStorage.setItem('authToken', token);
         return response.data;
     } catch (error) {
         console.error('Error signing up:', error);
@@ -14,6 +17,8 @@ export const signUp = async (userData: SignUpData) => {
 export const login = async (userData: LoginData) => {
     try {
         const response = await apiClient.post('/auth/login', userData);
+        const { token } = response.data;
+        await AsyncStorage.setItem('authToken', token);
         return response.data;
     } catch (error) {
         console.error('Error logging in:', error);
